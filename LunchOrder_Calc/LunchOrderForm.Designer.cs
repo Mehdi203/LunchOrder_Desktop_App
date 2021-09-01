ed783c043b43d917f94e9cfad34a7b7920e5bf4e
M <m.mehdinoroozi@gmail.com>
2021-08-31 18:15:02 -0600

    Mehdi

---
+++ LunchOrder_Calc/LunchOrderForm.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace LunchOrder_Calc
+{
+    partial class Order
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Order));
+            this.groupBoxMainCourse = new System.Windows.Forms.GroupBox();
+            this.radioButtonSalad = new System.Windows.Forms.RadioButton();
+            this.radioButtonPizza = new System.Windows.Forms.RadioButton();
+            this.radioButtonHam = new System.Windows.Forms.RadioButton();
+            this.groupBoxOrderTotal = new System.Windows.Forms.GroupBox();
+            this.labelOrderTotal = new System.Windows.Forms.Label();
+            this.textBoxOrderTotal = new System.Windows.Forms.TextBox();
+            this.labelTax = new System.Windows.Forms.Label();
+            this.textBoxTax = new System.Windows.Forms.TextBox();
+            this.labelSubTotal = new System.Windows.Forms.Label();
+            this.textBoxSubTotal = new System.Windows.Forms.TextBox();
+            this.buttonPlaceOrder = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBoxMainCourse.SuspendLayout();
+            this.groupBoxOrderTotal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxMainCourse
+            // 
+            this.groupBoxMainCourse.BackColor = System.Drawing.Color.Khaki;
+            this.groupBoxMainCourse.Controls.Add(this.radioButtonSalad);
+            this.groupBoxMainCourse.Controls.Add(this.radioButtonPizza);
+            this.groupBoxMainCourse.Controls.Add(this.radioButtonHam);
+            this.groupBoxMainCourse.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBoxMainCourse.Location = new System.Drawing.Point(21, 31);
+            this.groupBoxMainCourse.Name = "groupBoxMainCourse";
+            this.groupBoxMainCourse.Size = new System.Drawing.Size(187, 115);
+            this.groupBoxMainCourse.TabIndex = 0;
+            this.groupBoxMainCourse.TabStop = false;
+            this.groupBoxMainCourse.Text = "Main Course";
+            // 
+            // radioButtonSalad
+            // 
+            this.radioButtonSalad.AutoSize = true;
+            this.radioButtonSalad.Location = new System.Drawing.Point(15, 81);
+            this.radioButtonSalad.Name = "radioButtonSalad";
+            this.radioButtonSalad.Size = new System.Drawing.Size(119, 25);
+            this.radioButtonSalad.TabIndex = 2;
+            this.radioButtonSalad.TabStop = true;
+            this.radioButtonSalad.Text = "Salad - $4.95";
+            this.radioButtonSalad.UseVisualStyleBackColor = true;
+            this.radioButtonSalad.CheckedChanged += new System.EventHandler(this.radioButtonSalad_CheckedChanged);
+            // 
+            // radioButtonPizza
+            // 
+            this.radioButtonPizza.AutoSize = true;
+            this.radioButtonPizza.Location = new System.Drawing.Point(15, 56);
+            this.radioButtonPizza.Name = "radioButtonPizza";
+            this.radioButtonPizza.Size = new System.Drawing.Size(116, 25);
+            this.radioButtonPizza.TabIndex = 1;
+            this.radioButtonPizza.TabStop = true;
+            this.radioButtonPizza.Text = "Pizza - $5.95";
+            this.radioButtonPizza.UseVisualStyleBackColor = true;
+            this.radioButtonPizza.CheckedChanged += new System.EventHandler(this.radioButtonPizza_CheckedChanged);
+            // 
+            // radioButtonHam
+            // 
+            this.radioButtonHam.AutoSize = true;
+            this.radioButtonHam.Location = new System.Drawing.Point(15, 31);
+            this.radioButtonHam.Name = "radioButtonHam";
+            this.radioButtonHam.Size = new System.Drawing.Size(161, 25);
+            this.radioButtonHam.TabIndex = 0;
+            this.radioButtonHam.TabStop = true;
+            this.radioButtonHam.Text = "Hamburger - $6.95";
+            this.radioButtonHam.UseVisualStyleBackColor = true;
+            this.radioButtonHam.CheckedChanged += new System.EventHandler(this.radioButtonHam_CheckedChanged);
+            // 
+            // groupBoxOrderTotal
+            // 
+            this.groupBoxOrderTotal.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBoxOrderTotal.Controls.Add(this.labelOrderTotal);
+            this.groupBoxOrderTotal.Controls.Add(this.textBoxOrderTotal);
+            this.groupBoxOrderTotal.Controls.Add(this.labelTax);
+            this.groupBoxOrderTotal.Controls.Add(this.textBoxTax);
+            this.groupBoxOrderTotal.Controls.Add(this.labelSubTotal);
+            this.groupBoxOrderTotal.Controls.Add(this.textBoxSubTotal);
+            this.groupBoxOrderTotal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBoxOrderTotal.Location = new System.Drawing.Point(21, 188);
+            this.groupBoxOrderTotal.Name = "groupBoxOrderTotal";
+            this.groupBoxOrderTotal.Size = new System.Drawing.Size(355, 227);
+            this.groupBoxOrderTotal.TabIndex = 2;
+            this.groupBoxOrderTotal.TabStop = false;
+            this.groupBoxOrderTotal.Text = "Order Total";
+            // 
+            // labelOrderTotal
+            // 
+            this.labelOrderTotal.AutoSize = true;
+            this.labelOrderTotal.Location = new System.Drawing.Point(25, 173);
+            this.labelOrderTotal.Name = "labelOrderTotal";
+            this.labelOrderTotal.Size = new System.Drawing.Size(87, 21);
+            this.labelOrderTotal.TabIndex = 5;
+            this.labelOrderTotal.Text = "Order Total";
+            // 
+            // textBoxOrderTotal
+            // 
+            this.textBoxOrderTotal.Location = new System.Drawing.Point(113, 161);
+            this.textBoxOrderTotal.Multiline = true;
+            this.textBoxOrderTotal.Name = "textBoxOrderTotal";
+            this.textBoxOrderTotal.ReadOnly = true;
+            this.textBoxOrderTotal.Size = new System.Drawing.Size(184, 44);
+            this.textBoxOrderTotal.TabIndex = 4;
+            // 
+            // labelTax
+            // 
+            this.labelTax.AutoSize = true;
+            this.labelTax.Location = new System.Drawing.Point(25, 111);
+            this.labelTax.Name = "labelTax";
+            this.labelTax.Size = new System.Drawing.Size(67, 21);
+            this.labelTax.TabIndex = 3;
+            this.labelTax.Text = "Tax (5%)";
+            // 
+            // textBoxTax
+            // 
+            this.textBoxTax.Location = new System.Drawing.Point(113, 99);
+            this.textBoxTax.Multiline = true;
+            this.textBoxTax.Name = "textBoxTax";
+            this.textBoxTax.ReadOnly = true;
+            this.textBoxTax.Size = new System.Drawing.Size(184, 44);
+            this.textBoxTax.TabIndex = 2;
+            // 
+            // labelSubTotal
+            // 
+            this.labelSubTotal.AutoSize = true;
+            this.labelSubTotal.Location = new System.Drawing.Point(25, 54);
+            this.labelSubTotal.Name = "labelSubTotal";
+            this.labelSubTotal.Size = new System.Drawing.Size(66, 21);
+            this.labelSubTotal.TabIndex = 1;
+            this.labelSubTotal.Text = "Subtoal:";
+            // 
+            // textBoxSubTotal
+            // 
+            this.textBoxSubTotal.Location = new System.Drawing.Point(113, 40);
+            this.textBoxSubTotal.Multiline = true;
+            this.textBoxSubTotal.Name = "textBoxSubTotal";
+            this.textBoxSubTotal.ReadOnly = true;
+            this.textBoxSubTotal.Size = new System.Drawing.Size(184, 44);
+            this.textBoxSubTotal.TabIndex = 0;
+            // 
+            // buttonPlaceOrder
+            // 
+            this.buttonPlaceOrder.BackColor = System.Drawing.Color.Lime;
+            this.buttonPlaceOrder.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonPlaceOrder.Location = new System.Drawing.Point(435, 200);
+            this.buttonPlaceOrder.Name = "buttonPlaceOrder";
+            this.buttonPlaceOrder.Size = new System.Drawing.Size(113, 39);
+            this.buttonPlaceOrder.TabIndex = 3;
+            this.buttonPlaceOrder.Text = "Place Order";
+            this.buttonPlaceOrder.UseVisualStyleBackColor = false;
+            this.buttonPlaceOrder.Click += new System.EventHandler(this.buttonPlaceOrder_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Red;
+            this.buttonExit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonExit.Location = new System.Drawing.Point(435, 343);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(113, 39);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.Yellow;
+            this.buttonReset.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonReset.Location = new System.Drawing.Point(435, 266);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(113, 39);
+            this.buttonReset.TabIndex = 5;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(580, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 362);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Order
+            // 
+            this.AcceptButton = this.buttonPlaceOrder;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.CancelButton = this.buttonExit;
+            this.ClientSize = new System.Drawing.Size(848, 440);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonPlaceOrder);
+            this.Controls.Add(this.groupBoxOrderTotal);
+            this.Controls.Add(this.groupBoxMainCourse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Order";
+            this.Text = "Lunch Order";
+            this.groupBoxMainCourse.ResumeLayout(false);
+            this.groupBoxMainCourse.PerformLayout();
+            this.groupBoxOrderTotal.ResumeLayout(false);
+            this.groupBoxOrderTotal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxMainCourse;
+        private System.Windows.Forms.RadioButton radioButtonSalad;
+        private System.Windows.Forms.RadioButton radioButtonPizza;
+        private System.Windows.Forms.RadioButton radioButtonHam;
+        private System.Windows.Forms.GroupBox groupBoxOrderTotal;
+        private System.Windows.Forms.Label labelOrderTotal;
+        private System.Windows.Forms.TextBox textBoxOrderTotal;
+        private System.Windows.Forms.Label labelTax;
+        private System.Windows.Forms.TextBox textBoxTax;
+        private System.Windows.Forms.Label labelSubTotal;
+        private System.Windows.Forms.TextBox textBoxSubTotal;
+        private System.Windows.Forms.Button buttonPlaceOrder;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LunchOrder_Calc/LunchOrderForm.cs
@@ -0,0 +1,314 @@
+﻿/******************************
+ This C# application that accepts a lunch order from the user
+ and calculates the order subtotal and total with 5% tax added.
+ ***************************/
+/******************************
+Created by:
+Mehdi Noroozi
+
+Date: August 31, 2021
+ ***************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LunchOrder_Calc
+{
+    public partial class Order : Form
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        GroupBox groupBoxAddOn = new GroupBox();
+
+
+        private void radioButtonHam_CheckedChanged(object sender, EventArgs e)
+        {
+            /******
+             * Added a group box for the Add-on menue when the main course is Hamburger.
+             * The items of this groupbox will chnage based on selection of main course.
+             * Define groupBoxAddOn
+            */
+            int counter = groupBoxAddOn.Controls.Count;
+
+            //Remove groupbox when deselecting a main course and select another main course 
+            for (int i = counter - 1; i >= 0; i--)
+            groupBoxAddOn.Controls.Remove(groupBoxAddOn.Controls[i]);
+
+            //adding groupbox when the user select a main course
+            this.Controls.Add(groupBoxAddOn);
+
+            groupBoxAddOn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            groupBoxAddOn.Location = new System.Drawing.Point(252, 31);
+            groupBoxAddOn.Name = "groupBoxAddOn";
+            groupBoxAddOn.Size = new System.Drawing.Size(252, 115);
+            groupBoxAddOn.TabIndex = 6;
+            groupBoxAddOn.TabStop = false;
+            groupBoxAddOn.Text = "Add-on items ($0.75/each)";
+            // 
+            // checkBox1: creating first add-on for the add on when the main course is hamburger
+            CheckBox checkBox1 = new CheckBox();
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(9, 81);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(113, 25);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "French Fries";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2:  creating second add-on for the add on when the main course is hamburger
+            CheckBox checkBox2 = new CheckBox();
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new System.Drawing.Point(9, 56);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new System.Drawing.Size(226, 25);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Ketchup, mustard, and mayo";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3:  creating third add-on for the add on when the main course is hamburger
+            CheckBox checkBox3 = new CheckBox();
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new System.Drawing.Point(9, 29);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new System.Drawing.Size(224, 25);
+            checkBox3.TabIndex = 0;
+            checkBox3.Text = "Lettuce, Tomato, and Onions";
+            checkBox3.UseVisualStyleBackColor = true;
+
+            groupBoxAddOn.Controls.Add(checkBox1);
+            groupBoxAddOn.Controls.Add(checkBox2);
+            groupBoxAddOn.Controls.Add(checkBox3);
+            //// 
+        }
+        /******
+ * Added a group box for the Add-on menue when the main course is Pizza.
+ * The items of this groupbox will chnage based on selection of main course.
+ * Define groupBoxAddOn
+*/
+        // Pizza Toppings
+
+        private void radioButtonPizza_CheckedChanged(object sender, EventArgs e)
+        {
+            // groupBoxAddOn
+            //
+            int counter = groupBoxAddOn.Controls.Count;
+            for (int i = counter - 1; i >= 0; i--)
+            groupBoxAddOn.Controls.Remove(groupBoxAddOn.Controls[i]);
+
+            this.Controls.Add(groupBoxAddOn);
+            
+
+            groupBoxAddOn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            groupBoxAddOn.Location = new System.Drawing.Point(252, 31);
+            groupBoxAddOn.Name = "groupBoxAddOn";
+            groupBoxAddOn.Size = new System.Drawing.Size(252, 115);
+            groupBoxAddOn.TabIndex = 6;
+            groupBoxAddOn.TabStop = false;
+            groupBoxAddOn.Text = "Add-on items ($0.5/each)";
+            // 
+            // checkBox1:creating first add-on for the add on when the main course is Pizza
+            CheckBox checkBox1 = new CheckBox();
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(9, 81);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(113, 25);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Pepperoni";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2: creating 2nd add-on for the add on when the main course is Pizza
+            CheckBox checkBox2 = new CheckBox();
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new System.Drawing.Point(9, 56);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new System.Drawing.Size(226, 25);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Sausage";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3:creating 3rd add-on for the add on when the main course is Pizza
+            CheckBox checkBox3 = new CheckBox();
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new System.Drawing.Point(9, 29);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new System.Drawing.Size(224, 25);
+            checkBox3.TabIndex = 0;
+            checkBox3.Text = "Olives";
+            checkBox3.UseVisualStyleBackColor = true;
+
+            groupBoxAddOn.Controls.Add(checkBox1);
+            groupBoxAddOn.Controls.Add(checkBox2);
+            groupBoxAddOn.Controls.Add(checkBox3);
+            ////
+        }
+        /******
+ * Added a group box for the Add-on menue when the main course is Salad.
+ * The items of this groupbox will chnage based on selection of main course.
+ * Define groupBoxAddOn
+*/
+        //Salad Toppings
+        private void radioButtonSalad_CheckedChanged(object sender, EventArgs e)
+        {
+            // groupBoxAddOn
+            //
+            int counter = groupBoxAddOn.Controls.Count;
+            for (int i = counter - 1; i >= 0; i--)
+                groupBoxAddOn.Controls.Remove(groupBoxAddOn.Controls[i]);
+
+            this.Controls.Add(groupBoxAddOn);
+
+
+            groupBoxAddOn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            groupBoxAddOn.Location = new System.Drawing.Point(252, 31);
+            groupBoxAddOn.Name = "groupBoxAddOn";
+            groupBoxAddOn.Size = new System.Drawing.Size(252, 115);
+            groupBoxAddOn.TabIndex = 6;
+            groupBoxAddOn.TabStop = false;
+            groupBoxAddOn.Text = "Add-on items ($0.25/each)";
+            // 
+            // checkBox1: creating first add-on for the add on when the main course is Salad
+            CheckBox checkBox1 = new CheckBox();
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(9, 81);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(113, 25);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Croutons";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2: creating 2nd add-on for the add on when the main course is Salad
+            CheckBox checkBox2 = new CheckBox();
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new System.Drawing.Point(9, 56);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new System.Drawing.Size(226, 25);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Bacon Bits";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3: creating 3rd add-on for the add on when the main course is Salad
+            CheckBox checkBox3 = new CheckBox();
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new System.Drawing.Point(9, 29);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new System.Drawing.Size(224, 25);
+            checkBox3.TabIndex = 0;
+            checkBox3.Text = "Bread Sticks";
+            checkBox3.UseVisualStyleBackColor = true;
+
+            groupBoxAddOn.Controls.Add(checkBox1);
+            groupBoxAddOn.Controls.Add(checkBox2);
+            groupBoxAddOn.Controls.Add(checkBox3);
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+/****
+ * ******
+ * ***Main Calcualtion Section
+ * ******
+ *///
+        private void buttonPlaceOrder_Click(object sender, EventArgs e)
+        {
+            decimal MainCoursePrice, TaxRate, SubTotal, AddonPrice=0.0m;
+
+            TaxRate = 0.05m;
+
+            if (radioButtonHam.Checked)
+            { 
+                MainCoursePrice = 6.95m;
+                AddonPrice = 0.75m;
+            }
+            else if (radioButtonPizza.Checked)
+            {
+                MainCoursePrice = 5.95m;
+                AddonPrice = 0.5m;
+            }
+
+            else
+            {
+                MainCoursePrice = 4.95m;
+                AddonPrice = 0.25m;
+            }
+
+            //Calculate number of add-on items
+            
+            int AddonQuantity = groupBoxAddOn.Controls.OfType<CheckBox>().Count(x => x.Checked);
+            
+
+            SubTotal = MainCoursePrice + AddonQuantity * AddonPrice;
+            textBoxSubTotal.Text = SubTotal.ToString("C");
+            //textBoxTax.Text = Math.Round((SubTotal*TaxRate),2).ToString("C");
+            //textBoxOrderTotal.Text = Math.Round((SubTotal + SubTotal * TaxRate),2).ToString("C");
+
+            /*****************
+             * Convert to method
+             */
+
+            //Define parametrs for the CalcTax method
+            decimal OrderTotalCalc, TaxCalc;
+
+            //output Tax and total order
+            CalcTax(ref SubTotal, out TaxCalc, out OrderTotalCalc);
+            
+            //Pass tax and total order to the form
+            textBoxTax.Text = TaxCalc.ToString("C");
+            textBoxOrderTotal.Text = OrderTotalCalc.ToString("C");
+        }
+
+        //Define a Method to calculate tax and total order
+        
+        public static void CalcTax(ref decimal SubTotal,  
+            out decimal TaxCalc, out decimal OrderTotalCalc, decimal TaxRate = 0.05m)
+        {
+            TaxCalc = Math.Round((TaxRate * SubTotal), 2); //calculate tax
+            OrderTotalCalc = Math.Round((SubTotal + SubTotal * TaxRate), 2); //calculate total order
+   
+        }
+
+        //Reset button
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+
+            //Clear calculation section
+
+            textBoxSubTotal.Clear();
+            textBoxTax.Clear();
+            textBoxOrderTotal.Clear();
+
+            //Clear all check items in add-on groupbox
+            foreach (CheckBox c in groupBoxAddOn.Controls.OfType<CheckBox>())
+            {
+                c.Checked = false;
+            }
+
+            radioButtonHam.Checked = true;
+
+
+            
+        }
+    }
+}
